52c6cd1035812d832df9782f00e169f14bffa723
JuanSMartinez <mart1304@purdue.edu>
2021-04-29 15:01:55 -0400

    added grayscale html

--- js/handler.js
+++ js/handler.js
@@ -3,5 +3,10 @@ function launch_line_length_3IFC()
     window.open("line_length_3IFC.html", "_self");
 }
 
+function launch_grayscale()
+{
+    window.open("grayscale.html", "_self");
+}
 
 document.getElementById("3IFC").onclick = launch_line_length_3IFC;
+document.getElementById("grayscale").onclick = launch_grayscale;
